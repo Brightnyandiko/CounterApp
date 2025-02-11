3f7c220c31a6d1f712a9d567efd7e333062efe6a
Brightnyandiko <carlsonbright98@gmail.com>
2025-02-11 12:24:23 +0300

    updated the app

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../counter_model.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -12,28 +15,36 @@ class MyHomePage extends StatefulWidget {
 class _HomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("You have pushed the button this many times"),
-            Text(
-                "$counter",
-                style: Theme.of(context).textTheme.headlineMedium,
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: incrementCounter,
-        tooltip: "Increment",
-        child: const Icon(Icons.add),
-      ),
+    return Consumer<CounterModel>(
+        builder: (context, value, child) => Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(widget.title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("You have pushed the button this many times"),
+                Text(
+                  value.count.toString(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                )
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              //get access to the counter class
+              final counter = context.read<CounterModel>();
+
+              //increment
+              counter.increment();
+            },
+            tooltip: "Increment",
+            child: const Icon(Icons.add),
+          ),
+        )
     );
   }
 }
